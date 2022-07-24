4e963a3cfb090e7b78a38f867c6e900319366c46
Jo Soo Yoon <sooyoon126@gmail.com>
2022-07-24 11:37:47 +0900

    mustache 적용 및 index 페이지 생성

---
+++ src/test/java/com/soob1/book/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.soob1.book.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	@DisplayName("메인페이지 로딩")
+	void index() {
+		// when
+		String body = restTemplate.getForObject("/", String.class);
+
+		// then
+		assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+	}
+}
